19ef3ac0a295fc29fe2a63d27c03c17d01e27478
abdelouadoudborji <abdelouadoudborji@gmail.com>
2021-03-01 11:09:53 +0100

    second one

---
+++ src/main/java/com/client/ClientService/ClientService/dao/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.client.ClientService.ClientService.dao;
+
+import com.client.ClientService.ClientService.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ClientRepository extends JpaRepository<Client,Long> {
+}
